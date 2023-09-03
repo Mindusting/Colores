864d748eecc43ade407eaee19c50d51a926626fa
AlainG181 <128253784+Mindusting@users.noreply.github.com>
2023-09-03 11:36:30 +0200

    Update colores.py

--- colores.py
+++ colores.py
@@ -2,29 +2,26 @@
 # COLORES #
 ###########
 
-colores = []
+colores = (
+    # Color default
+    "\033[39;49m", #(0) default
 
-# Color default
-colores.append("\033[39;49m") #(0) default
+    "\033[30m", #(1)  negro
+    "\033[31m", #(2)  rojo
+    "\033[32m", #(3)  verde
+    "\033[33m", #(4)  amarillo
+    "\033[34m", #(5)  azul
+    "\033[35m", #(6)  morado
+    "\033[36m", #(7)  cian
+    "\033[37m", #(8)  blanco
 
-# Colores normales
-colores.append("\033[30m") #(1)  negro
-colores.append("\033[31m") #(2)  rojo
-colores.append("\033[32m") #(3)  verde
-colores.append("\033[33m") #(4)  amarillo
-colores.append("\033[34m") #(5)  azul
-colores.append("\033[35m") #(6)  morado
-colores.append("\033[36m") #(7)  cian
-colores.append("\033[37m") #(8)  blanco
-
-# Colores claros
-colores.append("\033[90m") #(9)  negro claro
-colores.append("\033[91m") #(10) rojo claro
-colores.append("\033[92m") #(11) verde claro
-colores.append("\033[93m") #(12) amarillo claro
-colores.append("\033[94m") #(13) azul claro
-colores.append("\033[95m") #(14) morado claro
-colores.append("\033[96m") #(15) cian claro
-colores.append("\033[97m") #(16) blanco claro
-
-colores = tuple(colores)
+    # Colores claros
+    "\033[90m", #(9)  negro claro
+    "\033[91m", #(10) rojo claro
+    "\033[92m", #(11) verde claro
+    "\033[93m", #(12) amarillo claro
+    "\033[94m", #(13) azul claro
+    "\033[95m", #(14) morado claro
+    "\033[96m", #(15) cian claro
+    "\033[97m"  #(16) blanco claro
+)
